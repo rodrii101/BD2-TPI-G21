fbb193f386b256aed1611afbd599c31cb1cc6ae6
unknown <nahuelsaucedo94@gmail.com>
2026-06-26 00:11:12 -0300

    SP para ver ventas a un cliente especifico

---
+++ SP_VentaPorCliente.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE InfoVentaPorCliente
+
+@idCliente INT
+
+AS
+BEGIN
+	BEGIN TRY
+	SELECT cli.Nombre,V.Fecha,mer.Descripcion,VD.Cantidad  FROM Cliente cli
+	INNER JOIN Carrito CAR ON CAR.IdCliente = CLI.Id
+	INNER JOIN Venta V ON V.IDCarrito = CAR.Id
+	INNER JOIN VentaDetalle VD ON VD.IdVenta = V.ID
+	INNER JOIN Mercaderia mer ON mer.Id = VD.IdMercaderia
+	WHERE cli.Id = @idCliente
+
+
+	END TRY
+	BEGIN CATCH
+		PRINT ERROR_MESSAGE()
+	END CATCH
+END;
