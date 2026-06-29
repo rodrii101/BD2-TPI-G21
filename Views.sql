7cb8eaefcf6be4d125b172fdd175b008be7aa1ea
Rodri <rodri.chocobar2005@gmail.com>
2026-06-29 13:30:52 -0300

    Views

--- Views.sql
+++ Views.sql
@@ -1,26 +1,37 @@
--- ALTER VIEW vista_topCompras
---     AS
---     SELECT TOP 25 
---     C.Id,
---     C.IdProveedor,
---     SUM(DC.Cantidad) AS CantidadTotal,
---     C.Total,
---     C.Fecha
---     FROM Compra C 
---     INNER JOIN DetalleCompra DC ON DC.IdCompra = C.Id
---     GROUP BY C.Id, C.IdProveedor, C.Fecha, C.Total
---     ORDER BY C.Total DESC, SUM(DC.Cantidad) DESC, C.Fecha DESC
+CREATE VIEW vista_topCompras
+    AS
+    SELECT TOP 50 
+    C.Id,
+    C.IdProveedor,
+    SUM(DC.Cantidad) AS CantidadTotalComprado,
+    C.Total,
+    C.Fecha
+    FROM Compra C 
+    INNER JOIN DetalleCompra DC ON DC.IdCompra = C.Id
+    GROUP BY C.Id, C.IdProveedor, C.Fecha, C.Total
+    ORDER BY C.Total DESC, SUM(DC.Cantidad) DESC, C.Fecha DESC
+GO
+CREATE VIEW vista_topVentas
+    AS
+    SELECT TOP 50
+    V.Id AS 'Id Venta',
+    V.IdMedioPago  AS MedioDePago,
+    SUM(DV.Cantidad) AS CantidadTotalVendido,
+    V.Fecha,
+    V.Total
+    FROM Venta V
+    INNER JOIN DetalleVenta DV ON DV.IdVenta = V.Id
+    GROUP BY V.Id, V.Fecha, V.IdMedioPago, V.Total
+    ORDER BY V.Total DESC, SUM(DV.Cantidad) DESC, V.Fecha DESC
+GO
 
--- CREATE VIEW vista_topCompras
---     AS
---     SELECT TOP 50
---     V.Id AS 'Id Venta',
---     V.IdMedioDePago AS 'Medio de pago',
---     SUM(DV.Cantidad) AS 'Cantidad total vendido',
---     V.Fecha,
---     V.Total
---     FROM Venta V
---     INNER JOIN DetalleVenta DV ON DV.IdVenta = V.Id
---     GROUP BY V.Id, V.Fecha, V.IdMedioDePago, V.Total
---     ORDER BY V.Total DESC, SUM(DV.Cantidad) DESC, V.Fecha DESC
-
+CREATE VIEW vista_stockBajoMercaderia
+    AS
+    SELECT TOP 100 Id AS 
+    IdMercaderia, 
+    Descripcion, 
+    Stock 
+    FROM Mercaderia
+    ORDER BY Stock ASC
+GO
+    
